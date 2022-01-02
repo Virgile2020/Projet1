d5f7c779999f474afcdfe825c5d2d2a00bcf8a5d
Virgile2020 <virgilegbeffan1@gmail.com>
2022-01-02 07:31:44 +0100

    Fin du projet

--- JS/Indix.js
+++ JS/Indix.js
@@ -48,10 +48,9 @@ document.querySelectorAll('.small-image-3').forEach(images =>{
     }
 })
 
-
-function updateClock() {
-    let now = new Date();
-    let dname = now.getDay(),
+function updateClock(){
+    var now = new Date();
+    var dname = now.getDay(),
         mo = now.getMonth(),
         dnum = now.getDate(),
         yr = now.getFullYear(),
@@ -59,13 +58,33 @@ function updateClock() {
         min = now.getMinutes(),
         sec = now.getSeconds(),
         pe = "AM";
-    let months = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
-    let week = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
-    let ids = ["dayname", "month", "daynum", "year", "hours", "minutes", "secondes", "periodes"];
-    
+
+        if (hou == 0) {
+            hou = 12;
+        }
+        if (hou > 12) {
+            hou = hou-12;
+            pe = "PM";
+        }
+        Number.prototype.pad = function (digits) {
+            for (var n = this.toString(); n.length < digits; n = 0 + n);
+            return n;
+                
+        }
+
+    var months = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
+    var weeks = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"];
+    var ids = ["dayname", "month", "daynum", "year", "hours", "minutes", "secondes", "periodes"];
+    var values = [weeks[dname] ,months[mo], dnum.pad(2), yr, hou.pad(2), min.pad(2), sec.pad(2), pe];
+
+    for (var i = 0; i < ids.length; i++) {
+
+        document.getElementById(ids[i]).firstChild.nodeValue = values[i];
+        
+    }
 }
 
-function initClock() {
+function initClock(){
     updateClock();
     window.setInterval('updateClock()', 1)
 }
